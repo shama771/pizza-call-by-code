410335165fa488b6e6c539eec9f42c8c8a8f7dff
shamanth <ushamanth771@gmail.com>
2022-08-28 19:40:21 +0530

    pizza code

---
+++ code.js
@@ -0,0 +1,71 @@
+// step1 : order pizza
+// step2 : accept order
+// step3 : prepare dought
+// step4 : start cuttings
+// step5 : put topings
+// step6 : Bake it 
+// step7 : pack it 
+// step8 : out for delivery 
+// step9 : order picked 
+// step10: pizza delivered to customer 
+
+
+setTimeout(
+    ()=>{
+        console.log("ordering pizza by customer")
+        setTimeout(
+            ()=>{
+                console.log("order accepted")
+                setTimeout(
+                    ()=>{
+                        console.log("preparing dought")
+                        setTimeout(
+                            ()=>{
+                                console.log("start cuttings vegetables")
+                                setTimeout(
+                                    ()=>{
+                                        console.log("adding topings N mozerlla cheese on it!")
+                                        setTimeout(
+                                            ()=>{
+                                                console.log("Baking it in otg pizza oven")
+                                                setTimeout(
+                                                    ()=>{
+                                                        console.log("pack it properly and put some toppings issue in bag")
+                                                        setTimeout(
+                                                            ()=>{
+                                                                console.log("out for delivery ")
+                                                                setTimeout(
+                                                                    ()=>{
+                                                                        console.log("order picked by delivery executive")
+                                                                        setTimeout(
+                                                                            ()=>{
+                                                                                console.log("pizza delivered to customer")
+                                                                            } ,5000
+                                                                        )
+                                                                    } ,1000
+                                                                )
+                                                            } ,1000
+                                                        )
+                                                    } ,9000
+                                                )
+                                            } ,3000
+                                        )
+                                    } ,4000
+                                )
+
+                            } ,7000
+                        )
+                    } ,2000
+                )
+            } ,1500
+        )
+    } ,1000
+
+)
+
+
+
+
+
+
+
